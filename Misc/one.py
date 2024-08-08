9f979a34bcd81b5d2e15ff6f6d74097ee728bf6f
thenatural86 <philip.smalls86@gmail.com>
2024-08-08 11:41:08 -0300

    folder refactor

---
+++ Misc/one.py
@@ -0,0 +1,11 @@
+def func():
+  print("FUNC() IN ONE.PY")
+  
+print("TOP LEVEL IN ONE.PY")
+
+if __name__ == '__main__':
+  print("ONE.PY is being run directly")
+  # RUN THE SCRIPT
+    # func()
+    # func2()
+    # ect...
